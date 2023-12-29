1aa8db462f3948311a406aa9c192876582ec7b37
YusupovJ <jamshudanamana@gmail.com>
2023-12-29 12:36:15 +0500

    fixed bug

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { login, logout, refresh, register } from "../controllers/authController.js";
-import roleGuard from "../middlewares/roleGuard.js";
 import { loginValidator, refreshValidator, registerValidator } from "../validators/authValidator.js";
+import authGuard from "../middlewares/authGuard.js";
 
 const authRoute = Router();
 
 authRoute.post("/register", ...registerValidator, register);
 authRoute.post("/login", ...loginValidator, login);
 authRoute.post("/refresh", ...refreshValidator, refresh);
-authRoute.post("/logout", roleGuard("user"), logout);
+authRoute.post("/logout", authGuard, logout);
 
 export default authRoute;
